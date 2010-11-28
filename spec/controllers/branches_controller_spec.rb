53dcd4d06bea0ab102a8329d70a3d8772a6a3738
Rory McKinley <rorymckinley@gmail.com>
2010-11-28 18:07:52 +0200

    Isolating controller tests

--- spec/controllers/branches_controller_spec.rb
+++ spec/controllers/branches_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe BranchesController do
+
+  before(:each) do
+    Branch.delete_all
+    @branch_1 = Branch.create! :name => 'Test 1'
+    @branch_2 = Branch.create! :name => 'Test 2'
+  end
+
   it "should list all the branches" do
-    Branch.should_receive(:find).with(:all)
     get :index
     response.should be_success
     response.should render_template("branches/index")
+    assigns(:branches).should eq [@branch_1, @branch_2]
   end
+
   it "should provide a way to create a new branch" do
     get :new
     response.should be_success
