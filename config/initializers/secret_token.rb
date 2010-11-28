1b83f2912f25b7d5ee8be9e41bbfa7eba631b4cd
Rory McKinley <rorymckinley@gmail.com>
2010-11-28 16:21:01 +0200

    Iniital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ikamva::Application.config.secret_token = '787b401b8a741f86080af3f9fc83e1f5d57be004f320b6fe42d79b846d867e9095a56e941617e0cca8588185ee161d9c2947488e5a9586e98990de271013f042'
