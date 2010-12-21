9001898e9113812a699e8b6cf32c50a5bf230140
Rory McKinley <rorymckinley@gmail.com>
2010-12-21 22:08:57 +0200

    Error handling on creating of attendance details

--- app/controllers/attendance_details_controller.rb
+++ app/controllers/attendance_details_controller.rb
@@ -7,7 +7,16 @@ class AttendanceDetailsController < ApplicationController
   def create
     participant = Participant.find(:first, :conditions => { :card_number => params[:participant]["card_number"]})
     event = Event.find(params[:event_id])
-    event.attendance_details.create :participant_id => participant.id, :status => params[:attendance_detail]["status"]
-    render :text => ''
+    branch = Branch.find(params[:branch_id])
+
+    unless participant
+      flash[:error] = "No participant with card number #{params[:participant]["card_number"]}"
+      redirect_to new_branch_event_attendance_detail_path(branch, event)
+      return
+    end
+
+    attendance_detail = event.attendance_details.create :participant_id => participant.id, :status => params[:attendance_detail]["status"]
+    flash[:attendance_detail] = "#{participant.name} has #{attendance_detail.status} credit"
+    redirect_to new_branch_event_attendance_detail_path(branch, event)
   end
 end
--- spec/controllers/attendance_details_controller_spec.rb
+++ spec/controllers/attendance_details_controller_spec.rb
@@ -22,7 +22,6 @@ describe AttendanceDetailsController do
 
   it "should allow attendance detail to be captured" do
     post :create, :branch_id => @branch_1.id, :event_id => @event.id, :participant => { "card_number" => '1234'}, :attendance_detail => { 'status' => 'full' }
-    response.should be_successful
 
     AttendanceDetail.find(:first, :conditions => { :participant_id => @participant.id, :status => 'full'}).should_not be_nil
     @event.reload.participants.should include @participant
@@ -32,5 +31,21 @@ describe AttendanceDetailsController do
     event2 = @branch_1.events.create! :purpose => 'homework', :start => Time.now + 1.minute, :end => Time.now + 1.hour
     post :create, :branch_id => @branch_1.id, :event_id => event2.id, :participant => { "card_number" => '1234'}, :attendance_detail => { 'status' => '' }
     AttendanceDetail.find(:first, :conditions => { :status => 'full' }).should_not be_nil
+  end
+
+  it "should return to the form for capturing attendance details after creation" do
+    post :create, :branch_id => @branch_1.id, :event_id => @event.id, :participant => { "card_number" => '1234'}, :attendance_detail => { 'status' => 'full' }
+    response.should redirect_to new_branch_event_attendance_detail_path(@branch_1, @event)
+  end
+
+  it "should display the status of the attendance detail created when returning to the form" do
+    post :create, :branch_id => @branch_1.id, :event_id => @event.id, :participant => { "card_number" => '1234'}, :attendance_detail => { 'status' => 'full' }
+    attendance_detail = AttendanceDetail.find(:first)
+    flash[:attendance_detail].should == "#{@participant.name} has #{attendance_detail.status} credit"
+  end
+
+  it "should return an error if the card number cannot be mapped to a participant" do
+    post :create, :branch_id => @branch_1.id, :event_id => @event.id, :participant => { "card_number" => '0000'}, :attendance_detail => { 'status' => 'full' }
+    flash[:error].should == "No participant with card number 0000"
   end
 end
