5bbdf07904bc0652c75cfef9145e899eb638a113
Rory McKinley <rorymckinley@gmail.com>
2011-02-13 08:41:20 +0200

    Comment out surplus member fields in preparation for moving to learner model

--- db/migrate/20110204192531_add_additional_fields_to_members.rb
+++ db/migrate/20110204192531_add_additional_fields_to_members.rb
@@ -4,22 +4,22 @@ class AddAdditionalFieldsToMembers < ActiveRecord::Migration
     add_column :members, :first_name, :string
     add_column :members, :surname, :string
     add_column :members, :gender, :string
-    add_column :members, :date_of_birth, :date
+    #add_column :members, :date_of_birth, :date
     add_column :members, :mobile_number, :string
-    add_column :members, :parent_guardian_number, :string
-    add_column :members, :disabilities, :boolean
+    #add_column :members, :parent_guardian_number, :string
+    #add_column :members, :disabilities, :boolean
     add_column :members, :address, :string
-    add_column :members, :school, :string
-    add_column :members, :grade, :string
+    #add_column :members, :school, :string
+    #add_column :members, :grade, :string
     add_column :members, :email, :string
     add_column :members, :id_number, :string
-    add_column :members, :citizenship, :string
-    add_column :members, :parent_guardian_1_relationship, :string
-    add_column :members, :parent_guardian_2_relationship, :string
-    add_column :members, :parent_guardian_1_job, :string
-    add_column :members, :parent_guardian_2_job, :string
-    add_column :members, :subjects, :text
-    add_column :members, :ikamvanite_family_members, :text
+    #add_column :members, :citizenship, :string
+    #add_column :members, :parent_guardian_1_relationship, :string
+    #add_column :members, :parent_guardian_2_relationship, :string
+    #add_column :members, :parent_guardian_1_job, :string
+    #add_column :members, :parent_guardian_2_job, :string
+    #add_column :members, :subjects, :text
+    #add_column :members, :ikamvanite_family_members, :text
   end
 
   def self.down
