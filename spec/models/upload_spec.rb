857818262956ce0607fff24da4710606e04ca6b2
Rory McKinley <rorymckinley@gmail.com>
2011-03-05 19:57:44 +0200

    Stripping of whitespace before saving branch names

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -17,7 +17,14 @@ describe Upload do
   end
   it "should import a set of combined data and exclude duplicate branch entries" do
     Branch.delete_all
-    Upload.import_combined("Branch 1\nBranch 2\nBranch 1")
+    Upload.import_combined("Branch 1\nBranch 2\n   Branch 1   ")
+    branches = Branch.all
+    branches.should have(2).elements
+    branches[1].name.should == "Branch 2"
+  end
+  it "should strip any trailing or leading whitespace from the branch name before saving it" do
+    Branch.delete_all
+    Upload.import_combined("Branch 1\n    Branch 2    ")
     branches = Branch.all
     branches.should have(2).elements
     branches[1].name.should == "Branch 2"
