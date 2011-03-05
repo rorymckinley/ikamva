08e098e474525711e32538e20659c4b6b92d2293
Rory McKinley <rorymckinley@gmail.com>
2011-03-05 21:45:39 +0200

    Combined uploads creates events and links them to branches

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -8,7 +8,8 @@ class Upload
   end
   def self.import_combined(content)
     FasterCSV.parse(content) do |combined_record|
-      Branch.create! :name => combined_record[0].strip unless Branch.find_by_name combined_record[0].strip
+      branch = Branch.find_by_name(combined_record[0].strip) || Branch.create!(:name => combined_record[0].strip)
+      branch.events.create! :purpose => combined_record[1]
     end
   end
 end
--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1,32 +1,51 @@
 require 'spec_helper'
 
 describe Upload do
-  it "should import csv data for branches and create the branches" do
+  before(:each) do
     Branch.delete_all
-    Upload.import_branches("Branch 1\nBranch 2")
-    branches = Branch.all
-    branches.should have(2).elements
-    branches[1].name.should == "Branch 2"
   end
-  it "should import a set of combined data and generate branch entries from the content" do
-    Branch.delete_all
-    Upload.import_combined("Branch 1\nBranch 2")
-    branches = Branch.all
-    branches.should have(2).elements
-    branches[1].name.should == "Branch 2"
+  context "branches" do
+    it "should import csv data for branches and create the branches" do
+      Upload.import_branches("Branch 1\nBranch 2")
+      branches = Branch.all
+      branches.should have(2).elements
+      branches[1].name.should == "Branch 2"
+    end
+    it "should import a set of combined data and generate branch entries from the content" do
+      Upload.import_combined("Branch 1\nBranch 2")
+      branches = Branch.all
+      branches.should have(2).elements
+      branches[1].name.should == "Branch 2"
+    end
+    it "should import a set of combined data and exclude duplicate branch entries" do
+      Upload.import_combined("Branch 1\nBranch 2\n   Branch 1   ")
+      branches = Branch.all
+      branches.should have(2).elements
+      branches[1].name.should == "Branch 2"
+    end
+    it "should strip any trailing or leading whitespace from the branch name before saving it" do
+      Upload.import_combined("Branch 1\n    Branch 2    ")
+      branches = Branch.all
+      branches.should have(2).elements
+      branches[1].name.should == "Branch 2"
+    end
   end
-  it "should import a set of combined data and exclude duplicate branch entries" do
-    Branch.delete_all
-    Upload.import_combined("Branch 1\nBranch 2\n   Branch 1   ")
-    branches = Branch.all
-    branches.should have(2).elements
-    branches[1].name.should == "Branch 2"
-  end
-  it "should strip any trailing or leading whitespace from the branch name before saving it" do
-    Branch.delete_all
-    Upload.import_combined("Branch 1\n    Branch 2    ")
-    branches = Branch.all
-    branches.should have(2).elements
-    branches[1].name.should == "Branch 2"
+  context "Events" do
+    before(:each) do
+      Event.delete_all
+    end
+    it "should import event data and link it to the branch specified in the record" do
+      Upload.import_combined("\"Branch 1\",\"tutorial\"\n\"Branch 2\",\"homework\"")
+      events = Event.all
+      events.should have(2).elements
+      events[1].purpose.should == 'homework'
+      events[0].branch.name.should == "Branch 1"
+      events[1].branch.name.should == "Branch 2"
+    end
+    it "should import event data and link it to a branch that already exist in the database" do
+      branch = Branch.create! :name => "Branch 1"
+      Upload.import_combined("\"Branch 1\",\"tutorial\"\n\"Branch 2\",\"homework\"")
+      Event.first.branch.should == branch
+    end
   end
 end
